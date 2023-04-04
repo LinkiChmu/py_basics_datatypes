322d8963413f7d38d2eb8822d0747d69fe51c1a7
chmutenkoa <alenachm@gmail.com>
2023-04-04 17:35:09 +0300

    part1: task_1 optimized slice, task_2: loop to generator

--- part_1/task_2.py
+++ part_1/task_2.py
@@ -3,10 +3,5 @@ The program asks the user for numbers in sequence (one at a time);
 after the first zero displays the sum of all previously entered numbers.
 
 '''
-
-numbers_sum = 0
-number = int(input('Введите число:\n'))
-while number != 0:
-    numbers_sum += number
-    number = int(input('Введите число:\n'))
-print(numbers_sum)
+print("Вводите числа по одному:")
+print(sum([int(i) for i in iter(input, '0')]))
