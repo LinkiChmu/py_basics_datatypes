7be3595c40449d0ef329bf8e59dfbea144888736
chmutenkoa <alenachm@gmail.com>
2023-04-05 15:27:48 +0300

    task_1 one line solution

--- part_2_dicts/task_1.py
+++ part_2_dicts/task_1.py
@@ -6,8 +6,5 @@ The program displays a set of unique geotags for all users.
 ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
-unique_tags = set()
-for user in ids.values():
-    for tag in user:
-        unique_tags.add(tag)
-print(unique_tags)
+
+print(set(sum(ids.values(), [])))
