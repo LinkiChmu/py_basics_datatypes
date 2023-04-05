751e472b07ae34b0cdbbb89557b71c6cde253645
chmutenkoa <alenachm@gmail.com>
2023-04-05 16:23:01 +0300

    task_5 var key removed

--- part_2_dicts/task_5.py
+++ part_2_dicts/task_5.py
@@ -1,16 +1,16 @@
-'''
+"""
 Given a list of some length.
 The program creates a dictionary, based on the original list,
 of such a nesting level as the length of the original list.
 
-'''
+"""
 my_list = ['a', 'b', 'c', 'd', 'e', 'f']
 new_list = list(my_list)
 value = new_list.pop()
 for i in range(len(new_list)):
     current_dict = {}
-    key = new_list.pop()
-    current_dict[key] = value
+    current_dict[new_list.pop()] = value
     value = current_dict
+current_dict = {}
 print(current_dict)
 
