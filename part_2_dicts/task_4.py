bd7bf125679c7c797750c702c9c755ad42706180
chmutenkoa <alenachm@gmail.com>
2023-04-11 17:57:27 +0300

    task_4 func.max()

--- part_2_dicts/task_4.py
+++ part_2_dicts/task_4.py
@@ -4,10 +4,5 @@ The program returns the name of channel with the highest sales volume.
 
 '''
 stats = {'facebook': 55, 'yandex': 115, 'vk': 120, 'google': 99, 'email': 42, 'ok': 98}
-max_volume = 0
-max_channel = ''
-for channel, volume in stats.items():
-    if volume > max_volume:
-        max_volume = volume 
-        max_channel = channel
-print(f'Максимальный объем продаж на рекламном канале: {max_channel}')
+
+print(max(stats, key=stats.get))
