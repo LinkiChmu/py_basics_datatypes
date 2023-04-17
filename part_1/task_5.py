dac148b6c4c726974ad105dd185d71c6941492e5
chmutenkoa <alenachm@gmail.com>
2023-04-17 12:43:16 +0300

    task_5 group of re.match added

--- part_1/task_5.py
+++ part_1/task_5.py
@@ -1,19 +1,20 @@
-'''
+"""
 There is a list with transport numbers.
 The program will check each number for validity (1 letter, 3 numbers, 2 letters, 2-3 numbers).
 Note that not all letters of the Cyrillic alphabet are used in transport numbers.
 
-'''
+"""
 
 import re
 
 letters_valid = {'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'}
 car_ids = ['А222ВС96', 'АБ22ВВ193']
+pattern = r'([АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2})(\d{2,3})'
 
-for car_id in car_ids:
+for id in car_ids:
     # checking the validity of letters and mask-checking
-    if len(set(re.findall(r'[а-яА-Я]', car_id)).difference(letters_valid)) == 0 \
-            and len(re.findall(r'[А-Я]\d{3}[А-Я]{2}\d{2,3}', car_id)[0]) == len(car_id):
-        print(f'Номер {car_id[:6]} валиден. Регион: {car_id[6:]}')
+    result = re.match(pattern, id)
+    if result:
+        print(f'Номер {result.group(1)} валиден. Регион: {result.group(2)}')
     else:
-        print(f'Номер {car_id} не валиден')
+        print(f'Номер {id} не валиден')
